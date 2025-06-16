6288cbc6581e82764de5c766243912c75aae069a
NayamulNirob <nayamulislam@gmail.com>
2025-06-16 23:49:34 +0600

    Add  ternary oparetor message and improve image URL randomization

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -6,11 +6,13 @@ function App() {
   const total =<>{5 + 6}</>
   const element = <h1>Hello World</h1>;
   const [imageUrl,setImageUrl] = useState("https://picsum.photos/100/200");
+  const isloggedIn = false;
   function imageChange() {
     alert("Image Changed");
-    setImageUrl(`https://picsum.photos/100/200?random=${Math.floor(Math.random() * 1000)}`);
+    setImageUrl(`https://picsum.photos/100/200?random=${Math.floor(Math.random() *1000)}`);
   }
   return <>
+  <h1>{isloggedIn?"Walcome":" Please login"} </h1>
     <h1>{element}</h1>
     <h1>....................,{total},..................</h1>
     <div>
