a41ed1f648ebf6e50056985f3c582841df9e0dc9
NayamulNirob <nayamulislam@gmail.com>
2025-06-16 22:52:14 +0600

    random image url Refactor App component to display total and update return structure

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 
 function App() {
+
+  const total =<>{5 + 6}</>
   // const element = <h1>Hello World</h1>;
-  return (
-  <>
- <h1>Hello</h1>
- <p>hi</p>
- </>
-  );
+  return <>
+
+   <h1>....................,{total},..................</h1>
+   <img src="https://picsum.photos/100/200" alt="Random" />
+   <button onClick={() => alert("Hello World")}style={{ backgroundColor: "green", color: "white", }}>
+      Click Me
+    </button>
+  </>;
 }
 
 export default App;
