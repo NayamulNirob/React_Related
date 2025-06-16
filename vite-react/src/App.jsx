2ee3d6c649c0eb0851b1ba77aefed0a454c940f4
NayamulNirob <nayamulislam@gmail.com>
2025-06-17 00:40:06 +0600

    Refactor total calculation and update image URL logic in App component

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -3,18 +3,20 @@ import "./App.css";
 
 function App() {
 
-  const total =<>{5 + 6}</>
+  let total1 =<>{Math.floor(Math.random() *1000)}</>
   const element = <h1>Hello World</h1>;
   const [imageUrl,setImageUrl] = useState("https://picsum.photos/100/200");
   const isloggedIn = false;
   function imageChange() {
+    const total= Math.floor(Math.random() * 1000);
     alert("Image Changed");
-    setImageUrl(`https://picsum.photos/100/200?random=${Math.floor(Math.random() *1000)}`);
+    setImageUrl(`https://picsum.photos/100/200?random=${total}`);
+    // window.location.reload();
   }
   return <>
   <h1>{isloggedIn?"Walcome":" Please login"} </h1>
     <h1>{element}</h1>
-    <h1>....................,{total},..................</h1>
+    <h1>{total1}</h1>
     <div>
         <img src={imageUrl} alt="Random" />
       </div>
