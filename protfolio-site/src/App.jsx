d4ae2d5730845d778437a49c30116b0ecc85f77c
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2025-06-23 16:15:40 +0600

    chore: comment out unused code sections to improve readability

--- protfolio-site/src/App.jsx
+++ protfolio-site/src/App.jsx
@@ -8,76 +8,76 @@ function App() {
     { title: "Project 2", description: "Description of Project 2",link:"b" },
     { title: "Project 3", description: "Description of Project 3",link:"c" },
   ];
-  const skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "Java",
-    "Dart",
-    "SQL",
-    "PL/SQL",
-    "React",
-    "Spring Boot",
-    "Angular",
-    "Oracle",
-    "Oracle Apex",
-    "Flutter",
-    "My SQL",
-    "Postgre SQL",
-  ];
-  const education = [
-    {
-      degree: "Bachelor of Science in Computer Science",
-      institution: "University of Technology",
-      year: "2020",
-    },
-    {
-      degree: "Master of Science in Software Engineering",
-      institution: "Tech University",
-      year: "2022",
-    },
-  ];
-  const experience = [
-    {
-      title: "Software Engineer",
-      company: "Tech Solutions",
-      duration: "2022 - Present",
-      description: "Developing web applications using React and Node.js.",
-    },
-    {
-      title: "Web Developer Intern",
-      company: "Web Innovations",
-      duration: "2021 - 2022",
-      description: "Assisted in building responsive websites.",
-    },
-  ];
-  const certifications = [
-    {
-      title: "Full Stack Web Development",
-      institution: "Online Academy",
-      year: "2023",
-    },
-    {
-      title: "JavaScript Fundamentals",
-      institution: "Code School",
-      year: "2021",
-    },
-  ];
-  const hobbies = [
-    "Coding",
-    "Reading Tech Blogs",
-    "Traveling",
-    "Photography",
-    "Gaming",
-  ];
-  const languages = ["English (Fluent)", "Bangla (Native)"];
-  const socialLinks = 
-    {github:"" ,
-    linkedin: "" ,
-     twitter: "" ,
-     facebook: "" ,
-     instagram: "" ,
-    };
+  // const skills = [
+  //   "HTML",
+  //   "CSS",
+  //   "JavaScript",
+  //   "Java",
+  //   "Dart",
+  //   "SQL",
+  //   "PL/SQL",
+  //   "React",
+  //   "Spring Boot",
+  //   "Angular",
+  //   "Oracle",
+  //   "Oracle Apex",
+  //   "Flutter",
+  //   "My SQL",
+  //   "Postgre SQL",
+  // ];
+  // const education = [
+  //   {
+  //     degree: "Bachelor of Science in Computer Science",
+  //     institution: "University of Technology",
+  //     year: "2020",
+  //   },
+  //   {
+  //     degree: "Master of Science in Software Engineering",
+  //     institution: "Tech University",
+  //     year: "2022",
+  //   },
+  // ];
+  // const experience = [
+  //   {
+  //     title: "Software Engineer",
+  //     company: "Tech Solutions",
+  //     duration: "2022 - Present",
+  //     description: "Developing web applications using React and Node.js.",
+  //   },
+  //   {
+  //     title: "Web Developer Intern",
+  //     company: "Web Innovations",
+  //     duration: "2021 - 2022",
+  //     description: "Assisted in building responsive websites.",
+  //   },
+  // ];
+  // const certifications = [
+  //   {
+  //     title: "Full Stack Web Development",
+  //     institution: "Online Academy",
+  //     year: "2023",
+  //   },
+  //   {
+  //     title: "JavaScript Fundamentals",
+  //     institution: "Code School",
+  //     year: "2021",
+  //   },
+  // ];
+  // const hobbies = [
+  //   "Coding",
+  //   "Reading Tech Blogs",
+  //   "Traveling",
+  //   "Photography",
+  //   "Gaming",
+  // ];
+  // const languages = ["English (Fluent)", "Bangla (Native)"];
+  // const socialLinks = 
+  //   {github:"" ,
+  //   linkedin: "" ,
+  //    twitter: "" ,
+  //    facebook: "" ,
+  //    instagram: "" ,
+  //   };
   const images = {
     profile: "https://example.com/profile.jpg",
     project1: "https://example.com/project1.jpg",
@@ -116,14 +116,14 @@ function App() {
                   <a href={project.link} target="_blank" rel="noprojecttillnowadded">View Project</a>
                 </div>
 
+              ),
+              (
+                <img
+                  src={images[`project${index + 1}`]}
+                  alt={project.title}
+                  className="project-image"
+                />
               )
-              // (
-              //   <img
-              //     src={images[`project${index + 1}`]}
-              //     alt={project.title}
-              //     className="project-image"
-              //   />
-              // )
             )
           )}
         </div>
