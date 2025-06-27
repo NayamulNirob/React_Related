a453ba5e7b614069319ec69f7d1d6bd5598b0e7c
NayamulNirob <nayamulislam@gmail.com>
2025-06-27 21:47:41 +0600

    refactor: use destructuring for props in ProfileCard component

--- vite-react/src/components/ProfileCard.jsx
+++ vite-react/src/components/ProfileCard.jsx
@@ -1,17 +1,37 @@
-function ProfileCard(props){
+// Using Destructuring to access props in a functional component
+function ProfileCard({ name, age, location, bio, hobbies, occupation, isMember }) {
 
     return(
-    <div className="profile-card">
-        <h3>Name: {props.name}</h3>
-        <p><b>Age: {props.age}</b></p>
-        <p><b>Location: {props.location}</b></p>
-        <p><b>Bio: {props.bio}</b></p>
-        <p><b>Hobbies: {props.hobbies.join(', ')}</b></p>
-        <p><b>Occupation: {props.occupation}</b></p>
-        <p><b>Status: {props.isMember? "Active":"Guest"}</b></p>
+    <div>
+        <h3>Name: {name}</h3>
+        <p><b>Age: {age}</b></p>
+        <p><b>Location: {location}</b></p>
+        <p><b>Bio: {bio}</b></p>
+        <p><b>Hobbies: {hobbies.join(', ')}</b></p>
+        <p><b>Occupation: {occupation}</b></p>
+        <p><b>Status: {isMember? "Active":"Guest"}</b></p>
     </div>
 
     );
 }
+
+
+
+//Using Dot Notation to access props in a functional component
+// function ProfileCard(props){
+
+//     return(
+//     <div className="profile-card">
+//         <h3>Name: {props.name}</h3>
+//         <p><b>Age: {props.age}</b></p>
+//         <p><b>Location: {props.location}</b></p>
+//         <p><b>Bio: {props.bio}</b></p>
+//         <p><b>Hobbies: {props.hobbies.join(', ')}</b></p>
+//         <p><b>Occupation: {props.occupation}</b></p>
+//         <p><b>Status: {props.isMember? "Active":"Guest"}</b></p>
+//     </div>
+
+//     );
+// }
 
 export default ProfileCard;
