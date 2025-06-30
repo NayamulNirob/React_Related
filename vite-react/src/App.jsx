63e174ae81a5d7c0211b2b70bd6ca404d5d6aec3
nayamulnirob <nayamulislam@gmail.com>
2025-06-30 16:08:40 +0600

    refactor: clean up whitespace and improve formatting in App component

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -5,7 +5,6 @@ import ProfileCard from "./components/ProfileCard";
 import Showalert from "./components/ShowAlart";
 import WelcomeMessage from "./components/WelcomeMessage";
 
-
 function App() {
   const now = new Date();
   const istrue = now.getHours() < 12;
@@ -13,35 +12,41 @@ function App() {
   return (
     <div>
       <WelcomeMessage name="Alice" />
-      <WelcomeMessage name="Bob"/>
-      <WelcomeMessage name ="Lion"/>
-      <Greeting istrue={istrue}/>
+      <WelcomeMessage name="Bob" />
+      <WelcomeMessage name="Lion" />
+      <Greeting istrue={istrue} />
       <MyButton />
-      <Showalert condition={istrue} message="Good Morning! Have a great day!"/>
-      <Showalert condition={!istrue} message="Good Evening! Hope you had a nice day!"/>
+      <Showalert condition={istrue} message="Good Morning! Have a great day!" />
+      <Showalert
+        condition={!istrue}
+        message="Good Evening! Hope you had a nice day!"
+      />
       <h2>Current Time: {now.toLocaleTimeString()}</h2>
 
-      <ProfileCard
-      name="John Doe"
-      age={30}
-      location="New York"
-      bio="A software developer with a passion for coding and technology."
-      hobbies={["Reading", "Traveling", "Gaming"]}
-      skills={["Java,", " JavaScript,", " React,", " Flutter"]}
-      occupation="Software Engineer"
-      isMember={true}
-      />,
+      <div className="app-container">
+        <ProfileCard
+          name="John Doe"
+          age={30}
+          location="New York"
+          bio="A software developer with a passion for coding and technology."
+          hobbies={["Reading", "Traveling", "Gaming"]}
+          skills={["Java,", " JavaScript,", " React,", " Flutter"]}
+          occupation="Software Engineer"
+          isMember={true}
+        />
+        ,
+        <ProfileCard
+          name="Jane Smith"
+          age={28}
+          location="San Francisco"
+          bio="A creative designer who loves art and innovation."
+          hobbies={["Painting", "Photography", "Hiking"]}
+          skills={["Photoshop,", " Illustrator,", " Figma"]}
+          occupation="Graphic Designer"
+          isMember={false}
+        />
+      </div>
 
-      <ProfileCard
-      name="Jane Smith"
-      age={28}
-      location="San Francisco"
-      bio="A creative designer who loves art and innovation."
-      hobbies={["Painting", "Photography", "Hiking"]}
-      skills={["Photoshop,", " Illustrator,", " Figma"]}
-      occupation="Graphic Designer"
-      isMember={false}
-      />
     </div>
   );
 }
